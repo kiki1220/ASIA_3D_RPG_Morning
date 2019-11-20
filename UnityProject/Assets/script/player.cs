9689c37caa5236620c9a8a29ab4394ee6eb5415b
kiki1220 <55470495+kiki1220@users.noreply.github.com>
2019-11-20 11:59:34 +0800

    1120

---
+++ UnityProject/Assets/script/player.cs
@@ -0,0 +1,19 @@
+﻿
+using UnityEngine;
+
+public class player : MonoBehaviour
+{
+    public string run;
+    public string jump;
+    public string attack;
+    public string hurt;
+    public string dead;
+    public string propneed;
+    public string propgain;
+
+    public bool deads;
+
+    public int hp;
+
+    }
+

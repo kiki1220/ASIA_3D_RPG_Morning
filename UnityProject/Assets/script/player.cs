8d6b473442b79dc2c3f09d576d280fb544928f73
kiki1220 <55470495+kiki1220@users.noreply.github.com>
2019-11-27 12:03:10 +0800

    1127

--- UnityProject/Assets/script/player.cs
+++ UnityProject/Assets/script/player.cs
@@ -3,17 +3,34 @@ using UnityEngine;
 
 public class player : MonoBehaviour
 {
-    public string run;
-    public string jump;
-    public string attack;
-    public string hurt;
-    public string dead;
-    public string propneed;
-    public string propgain;
+    [Header("動畫參數名稱")]
+    public string parrun = "跑步開關";
+    public string parjump = "跳耀開關";
+    public string paratk = "攻擊開關";
+    public string pardamage = "受傷";
+    public string pardead = "死亡";
 
-    public bool deads;
+    [Header("道具")]
+    public int countneed = 10;
+    public int countcurrent;
 
-    public int hp;
+    [Header("數值")]
+    public bool isdead;
+    public float hp = 100;
 
+    //遊戲開始
+    private void Start()
+    {
+        
     }
+
+    //更新(約一秒更新60次)
+    private void Update()
+    {
+        
+    }
+
+    
+}
+
 
